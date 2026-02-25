6f3f1b69669a5c145ce54885f0a144e97789c498
Harsh Soni <harshsoni6011@gmail.com>
2026-02-25 21:05:13 +0530

    Adds google map in contact-us section

--- backend/Contact.api/obj/Debug/net8.0/Contact.api.AssemblyInfo.cs
+++ backend/Contact.api/obj/Debug/net8.0/Contact.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contact.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c670e186f7613383020c0129eeae9f9dc0e9e3cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c10dd4eb594d3d93e6bae60ddf0397d4bee59f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contact.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contact.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/components/sections/ContactUs.tsx
+++ client/src/components/sections/ContactUs.tsx
@@ -184,7 +184,7 @@ const ContactUs = () => {
                   </div>
                   <div className="ml-4">
                     <h4 className="text-lg font-medium text-[#0f2549]">Phone</h4>
-                    <p className="text-gray-600">+1 (800) 123-4567</p>
+                    <p className="text-gray-600">+91 8980753743</p>
                   </div>
                 </div>
                 
@@ -222,13 +222,16 @@ const ContactUs = () => {
               transition={{ duration: 0.6, delay: 0.2 }}
             >
               {/* Google Map would be embedded here */}
-              <div className="w-full h-full bg-gray-200 rounded-lg overflow-hidden">
-                <img 
-                  src="https://images.unsplash.com/photo-1618606758667-2523a330da83?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" 
-                  alt="Map Location" 
-                  className="w-full h-full object-cover"
-                />
-              </div>
+            <div className="w-full h-full bg-gray-200 rounded-lg overflow-hidden">
+              {/* <iframe
+                title="Google Map"
+                src="https://www.google.com/maps?q=Gandhidham&output=embed"
+                className="w-full h-full"
+                loading="lazy"
+                allowFullScreen
+              ></iframe> */}
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d917.7215666590984!2d70.13548828661443!3d23.064630891703466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3950b98cd58c81eb%3A0xc1d0f8b8876d6d89!2sKrishna%20Logistics!5e0!3m2!1sen!2sin!4v1772033508574!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
             </motion.div>
           </div>
         </div>
